98078b6813e2ae9a9208969db61c63a2c9b2b05a
tajidyakub <tajid.yakub@gmail.com>
2023-12-25 01:09:13 +0700

    Service basic function

--- tests/Feature/FunctionTest.php
+++ tests/Feature/FunctionTest.php
@@ -1,13 +1,21 @@
 <?php
 
 use Illuminate\Http\Client\PendingRequest;
-use Tjx\IpLoc\IpLoc;
+use Illuminate\Http\Request;
 use Tjx\IpLoc\IpLocApi;
 
 it("Provides `IpLocApi` facade to be accessible constructing `IpLoc` class.", function () {
     $c = IpLocApi::getClient();
 
     expect($c)->toBeInstanceOf(PendingRequest::class);
+});
+
+it("Has `getIpAddress` method accepting Request class and get ip address from x-forwarded-for or request()->ip()", function () {
+
+    $this->withHeaders(['x-forwarded-for' => '1.1.1.1,2.2.22']);
+    
+    dump($this->headers);
+
 });
 
 //it("", function () {});
